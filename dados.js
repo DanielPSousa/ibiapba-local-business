75110cb7e15090c92e55acb9d39c49c652567f85
DanielPSousa <47143209+DanielPSousa@users.noreply.github.com>
2024-09-10 15:08:07 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -4,28 +4,28 @@ let dados = [
         descricao: "Com mais de 35 anos de história, o Martmag oferece uma ampla variedade de produtos e serviços, sempre buscando atender às necessidades dos clientes com qualidade e preços competitivos. Presente em diversas cidades da região, o Martmag se destaca por sua expansão constante e por oferecer uma experiência de compra completa e agradável.",
         link: "https://www.instagram.com/supermercadomartmagguaraciaba/",
         tags: "supermercado mercado martmag",
-        imagem: "_img/martmag.jpeg" // Nome do arquivo da imagem
+        imagem: "martmag.jpeg" // Nome do arquivo da imagem
     },
     {
         titulo: "Doni's Pizza",
         descricao: "A Doni's Pizza & Massas é um dos destaques gastronômicos de Guaraciaba do Norte. Com um ambiente aconchegante e um atendimento impecável, a pizzaria conquista seus clientes com pizzas deliciosas e massas irresistíveis. Seus pratos são preparados com ingredientes frescos e de alta qualidade, garantindo um sabor único e autêntico. A Doni's é a escolha perfeita para quem busca uma experiência gastronômica completa e memorável.",
         link: "https://www.empresa1.com.br",
         tags: "pizza, massa, lanche",
-        imagem: "_img/logo-donispizza.png" // Nome do arquivo da imagem
+        imagem: "logo-donispizza.png" // Nome do arquivo da imagem
     },
     {
         titulo: "Fazenda Gospel",
         descricao: " O melhor Hotel Fazenda do Brasil <br> 🌨️ Na Serra mais fria do Ceará, Guaraciaba do Norte.📍<br> 🌳 Descanso, Diversão e Lazer👇 <br> Faça sua Reserva Agora 👇",
         link: "https://www.empresa2.com.br",
         tags: "fazenda, turismo, passeio, gospel",
-        imagem: "_img/fazendagospel.png" // Nome do arquivo da imagem
+        imagem: "fazendagospel.png" // Nome do arquivo da imagem
     },
     {
         titulo: "Hive Honey",
         descricao: "Saboreie a natureza pura da Serra da Ibiapaba com o mel Hive Honey. Produzido por abelhas que coletam o néctar das flores mais exuberantes da região, nosso mel oferece um sabor único e inigualável. Cada colherada é uma experiência sensorial, com notas florais e um toque de melato que o tornam perfeito para adoçar seus momentos especiais.",
         link: "https://whttps://www.instagram.com/melhivehoney/.com.br",
         tags: "mel, doce, guaraciaba, hive, hivehoney",
-        imagem: "_img/hive-honey.jpg" // Nome do arquivo da imagem
+        imagem: "hive-honey.jpg" // Nome do arquivo da imagem
     },
     // ... outros objetos para as empresas 3 e 4
 ];
